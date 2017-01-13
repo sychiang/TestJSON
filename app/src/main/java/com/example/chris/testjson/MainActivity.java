811cfb7c8890b3645ad97e78838bb1b55786fa16
sychiang <chris.chiang13@gmail.com>
2017-01-13 11:11:52 +0800

    v.testingfrMac

--- app/src/main/java/com/example/chris/testjson/MainActivity.java
+++ app/src/main/java/com/example/chris/testjson/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
         //Testing GitHub
+        //Testing GitHub from Mac
 
         AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.get(url)
